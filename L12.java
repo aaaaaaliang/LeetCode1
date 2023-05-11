f864b8e5dc1d6b3fa68038f5b93b660f9fa11a4c
zhangxueliang <117182742+aaaaaaliang@users.noreply.github.com>
2023-05-11 08:21:57 +0800

    Create L12.java

---
+++ L12.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null){
+            return head;
+        }
+     ListNode pre=head;
+     while(pre.next!=null){
+         if(pre.val==pre.next.val){
+             pre.next=pre.next.next;
+         }else{
+             pre=pre.next;
+         }
+     }
+     return head;
+    }
+}
+
+/*
+给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表。
+
+示例 1：
+输入：head = [1,1,2]
+输出：[1,2]
+示例 2：
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+ 
+提示：
+链表中节点数目在范围 [0, 300] 内
+-100 <= Node.val <= 100
+题目数据保证链表已经按升序排列
+
+思路：创建一个指针指向头节点，先判断是否为空，为空返回，如果节点相等，断开节点。
+*/
