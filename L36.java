389aa27c186e1c3ba15c52634349e745865c12e9
zhangxueliang <117182742+aaaaaaliang@users.noreply.github.com>
2023-06-07 20:15:49 +0800

    Create L36.java

---
+++ L36.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
+/*
+题目描述49. 字母异位词分组
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+示例 1:
+输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+示例 2:
+输入: strs = [""]
+输出: [[""]]
+示例 3:
+输入: strs = ["a"]
+输出: [["a"]]
+思路：先排序，哈希映射检查是否存在，存在添加进去，不存在创建新的
+*/
